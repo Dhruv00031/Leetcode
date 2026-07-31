2300650f53603d8230b03f493c3324447144cab9
Dhruv Bharadwaj <157914896+Dhruv00031@users.noreply.github.com>
2026-07-31 10:36:16 +0530

    Time: 34 ms (22.4%), Space: 26.9 MB (46.22%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        unordered_map<char, int> mp;
+
+        for (char ch : word) {
+            mp[ch]++;
+        }
+
+        vector<int> freq;
+
+        for(auto it : mp){
+            freq.push_back(it.second);
+        }
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int pushes = 0;
+        for (int i = 0; i < freq.size(); i++) {
+            pushes += freq[i] * ((i / 8) + 1);
+        }
+        return pushes;
+    }
+};
